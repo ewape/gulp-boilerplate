e2f5961cfb8291077787da902ff6e16238365668
Ewa Pettke <ewapettke@gmail.com>
2016-05-15 16:35:07 +0200

    Gulp starter

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp   = require('gulp'),
+    exists = require('path-exists').sync,
+    autoprefixer = require('gulp-autoprefixer'),
+    cache = require('gulp-cache'),
+    concat = require('gulp-concat'),
+    cssnano = require('gulp-cssnano'),
+    imagemin = require('gulp-imagemin'),
+    jshint = require('gulp-jshint'),
+    livereload = require('gulp-livereload'),
+    notify = require('gulp-notify'),
+    rename = require('gulp-rename'),
+    sass   = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    mainBowerFiles = require('main-bower-files');
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/scss/**/*.scss', ['styles']);
+  gulp.watch('src/images/**/*', ['images']);
+  gulp.watch('bower_components/**/*', ['bower-copy-min']);
+  livereload.listen();
+  gulp.watch(['dist/**', 'index.html']).on('change', livereload.changed);
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/scss/**/*.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({ style: 'compact' }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+	.pipe(sourcemaps.init())
+	.pipe(concat('script.min.js'))
+    .pipe(jshint())
+    .pipe(uglify())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('bower-copy-min', function() {
+  var bowerWithMin = mainBowerFiles().map( function(path, index, arr) {
+    var newPath = path.replace(/.([^.]+)$/g, '.min.$1');
+    return exists(newPath) ? newPath : path;
+  });
+  gulp.src(bowerWithMin).pipe(gulp.dest('./src/vendor/'));
+});
