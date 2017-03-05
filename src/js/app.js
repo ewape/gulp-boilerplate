2f364430182dbadd1b6a18d7e46059089ad7b843
Ewa <ewapettke@gmail.com>
2017-03-05 20:33:35 +0100

    update mobile

--- src/js/app.js
+++ src/js/app.js
@@ -37,21 +37,19 @@
     })();
 
     app.scroll = (function() {
-        let $scrollConntainer = $('main'),
+        let $scrollContainer = $('main'),
             sectionSelector = 'section',
             menuSelector = '.main-menu',
-            $containers = $scrollConntainer.find('.container'),
-            sectionsArr = Array.apply(null, document.querySelectorAll(sectionSelector)),
-            menuEL = document.querySelector(menuSelector),
+            iconHiddenClass = 'icon-hidden',
+            body = document.body,
+            $containers = $scrollContainer.find('.container'),
+            sectionsArr = Array.apply(null, body.querySelectorAll(sectionSelector)),
+            menuEL = body.querySelector(menuSelector),
             currentSection = {},
-            bodyStyles = window.getComputedStyle(document.body),
+            bodyStyles = window.getComputedStyle(body),
             currentColor = bodyStyles.getPropertyValue('--current-color');
 
-        const getCurrentSection = () => {
-            let currentElem = sectionsArr.filter((el) => el.classList.contains('active'));
-            currentSection.id = currentElem[0].id;
-            currentSection.index = sectionsArr.indexOf(currentElem);
-        };
+        const getCurrentSection = () => sectionsArr.filter((el) => el.classList.contains('active'))[0];
 
         const updateCurrentSection = (index) => {
             currentSection.id = sectionsArr[index].id;
@@ -74,8 +72,12 @@
             links.map((link) => link.addEventListener('click', menuClick));
         };
 
+        const hideIcon = () => body.classList.add(iconHiddenClass);
+
+        const showIcon = () => body.classList.remove(iconHiddenClass);
+
         function init() {
-            $scrollConntainer.pagepiling({
+            $scrollContainer.pagepiling({
                 direction: 'horizontal',
                 navigation: false,
                 menu: '.main-menu',
@@ -85,16 +87,30 @@
                 onLeave: function(index, nextIndex, direction) {
                     //console.log(index, nextIndex, direction);
                     updateCurrentColor(nextIndex - 1);
+
                     //remove animation class
                     //$containers.eq(index - 1).removeClass('show-content');
                 },
                 afterLoad: function(anchorLink, index) {
                     updateCurrentSection(index - 1);
+
+                    if (index === 1) {
+                        hideIcon();
+                    }
+
+                    else {
+                        showIcon();
+                    }
                     //add animation class
                     //$containers.eq(index - 1).addClass('show-content');
                 },
                 afterRender: function() {
-                    getCurrentSection();
+                    let currentId = getCurrentSection().id,
+                    index = getAnchors().indexOf(currentId);
+                    //console.log(getCurrentSection().id);
+                    if (index === 0) {
+                        hideIcon();
+                    }
                 },
             });
 
