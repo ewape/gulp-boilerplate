dd456f63604ac08d2040039445ec9de9a9cb08ad
Ewa <ewapettke@gmail.com>
2017-03-10 17:50:27 +0100

    add cookies

--- src/js/vendors/cookies.js
+++ src/js/vendors/cookies.js
@@ -5,8 +5,54 @@
 
     app.cookies = (function() {
 
+        var cookiesMessage = {
+            text: 'Strona stosuje pliki cookies. Korzystanie ze strony oznacza zgodę na wykorzystanie plików cookies zgodnie z',
+            acceptLabel: 'Akceptuj',
+            linkLabel: 'Polityką cookies',
+            containerId: 'cookies-message-container',
+            acceptId: 'accept-cookies',
+            url: 'cookies-url'
+        };
+
+        function readCookie() {
+            return localStorage.getItem('cookies-accepted');
+        }
+
+        function setCookie() {
+            var acceptBtn = document.getElementById(cookiesMessage.acceptId),
+                messageContainer = document.getElementById(cookiesMessage.containerId);
+            acceptBtn.addEventListener('click', function(e) {
+                e.preventDefault();
+                localStorage.setItem('cookies-accepted', 1);
+                messageContainer.classList.add('cookies-accepted');
+            });
+        }
+
+        function addCookieMessage() {
+            var messageContainer = document.createElement('div');
+            messageContainer.setAttribute('id', cookiesMessage.containerId);
+            messageContainer.setAttribute('class', cookiesMessage.containerId);
+            var htmlCode = '<div class="container cookies-container"><p class="cookies-message-text">' +
+                cookiesMessage.text +
+                ' <a class="cookies-link" target="_blank" href="' + location.protocol + '//' + location.host + cookiesMessage.url + '">' + cookiesMessage.linkLabel +
+                '</a>. </p><button type="button" id="' + cookiesMessage.acceptId + '" class="btn cookies-accept-btn">' +
+                cookiesMessage.acceptLabel +
+                '</button></div>';
+
+            messageContainer.innerHTML = htmlCode;
+            document.body.appendChild(messageContainer);
+            setCookie();
+        }
+
+        function checkCookiesAccepted() {
+            var cookie = readCookie();
+            if (cookie === null) {
+                addCookieMessage();
+            }
+        }
+
         function init() {
-            console.log('cookies - coming soon');
+            checkCookiesAccepted();
         }
 
         return {
@@ -14,7 +60,5 @@
         };
 
     })();
-
-    app.cookies.init();
 
 })();
