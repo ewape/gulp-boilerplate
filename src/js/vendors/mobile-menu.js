9555853f8847d7281d9d8527b815bdc648ed6548
Ewa <ewapettke@gmail.com>
2017-03-17 18:19:13 +0100

    change variables order

--- src/js/vendors/mobile-menu.js
+++ src/js/vendors/mobile-menu.js
@@ -7,9 +7,9 @@
         const button = document.querySelector('.nav-btn'),
             body = document.body,
             menuSelector = '.main-menu',
+            openMenuClass = 'menu-open',
             menu = body.querySelector(menuSelector),
-            transitionDelay = 300,
-            openMenuClass = 'menu-open';
+            transitionDelay = 300;
 
         const openMenu = () => body.classList.add(openMenuClass);
         const closeMenu = () => body.classList.remove(openMenuClass);
@@ -20,6 +20,7 @@
                 closeMenu();
             }, transitionDelay);
         };
+
         const addEvents = () => {
             let links = Array.apply(null, menu.childNodes);
             links.map((link) => link.addEventListener('click', menuClick));
