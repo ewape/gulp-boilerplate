d282479fdee9d382ac0e011647fe787e3567c72b
Ewa <ewapettke@gmail.com>
2017-03-25 09:51:00 +0100

    format app.js markup
    # Conflicts:
    #	src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,7 @@
     app.colorModule = (() => {
 
         const generateColor = () => '#' + Math.random().toString(16).slice(2, 8);
-
-        const updateColorVar = () => {
-            console.log('some stuff');
-            document.body.style.setProperty('--color-var', generateColor());
-        };
-
+        const updateColorVar = () => document.body.style.setProperty('--color-var', generateColor());
         const init = () => updateColorVar();
 
         return {
